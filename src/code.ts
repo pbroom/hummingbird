eb82d609d62dd9a7c403058915a371a4d5b7015e
pbroom <116581966+pbroom@users.noreply.github.com>
2023-06-29 12:24:22 -0400

    set up webpack

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,5 @@
+import { createRectangles } from "./rectangle-module"
+
 // This plugin will open a window to prompt the user to enter a number, and
 // it will then create that many rectangles on the screen.
 
@@ -17,11 +19,7 @@ figma.ui.onmessage = msg => {
   if (msg.type === 'create-rectangles') {
     const nodes: SceneNode[] = [];
     for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+      createRectangles(i,nodes);
     }
     figma.currentPage.selection = nodes;
     figma.viewport.scrollAndZoomIntoView(nodes);
